52e2b318161cb20ac3aca484ce124ed195f1de56
jenshadlich <jenshadlich@googlemail.com>
2015-02-17 16:29:09 +0100

    add dependency to Apache XML RPC

--- src/main/java/de/jeha/opennebula/client/SimpleClient.java
+++ src/main/java/de/jeha/opennebula/client/SimpleClient.java
@@ -1,7 +1,18 @@
 package de.jeha.opennebula.client;
 
+import org.apache.xmlrpc.client.XmlRpcClient;
+
 public class SimpleClient {
-    private String basicAuth;
-    private String endPointUrl;
-    //private XmlRpcClient client;
+    private final String basicAuth;
+    private final String endPointUrl;
+    private XmlRpcClient client;
+
+    public SimpleClient(String basicAuth, String endPointUrl) {
+        this.basicAuth = basicAuth;
+        this.endPointUrl = endPointUrl;
+    }
+
+    public Response call(){
+        return null;
+    }
 }
